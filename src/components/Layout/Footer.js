d8454f3758085dd2d6765bff0f210b06d2fc096a
William Eduardo Meza <williameduardomeza@gmail.com>
2020-05-31 16:57:28 -0500

    feat: style footer and add social icons

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,22 +1,42 @@
 import React from 'react';
-
-import { rhythm } from 'utils/typography';
+import './Footer.css';
+import github from '../../../content/assets/icons/github.svg';
+import linkedin from '../../../content/assets/icons/linkedin.svg';
+import twitter from '../../../content/assets/icons/twitter.svg';
 
 function Footer() {
   return (
-    <footer
-      style={{
-        marginTop: rhythm(2.5),
-        paddingTop: rhythm(1),
-        textAlign: 'center',
-      }}
-    >
-      <p>
-        Made with{' '}
-        <a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer">
-          gatsby
-        </a>
-      </p>
+    <footer>
+      <p>Copyright © 2020 by William Eduardo Meza</p>
+      <ul
+        style={{
+          display: 'flex',
+          listStyle: 'none',
+          margin: '0',
+          width: '100px',
+          justifyContent: 'space-between',
+        }}
+      >
+        <li>
+          <a href="https://github.com/wemf" target="_blank" rel="noopener noreferrer nofollow">
+            <img src={github} alt="github wmeza account" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://linkedin.com/in/william-meza"
+            target="_blank"
+            rel="noopener noreferrer nofollow"
+          >
+            <img src={linkedin} alt="linkedin wmeza account" />
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/wemeza" target="_blank" rel="noopener noreferrer nofollow">
+            <img src={twitter} alt="twitter wmeza account" />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }
