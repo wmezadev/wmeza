4ce961e15cf30914343bf22f2bf37fc181cd29f0
William Eduardo Meza <williameduardomeza@gmail.com>
2020-05-31 21:10:03 -0500

    fix: responsive menu in safari

--- src/components/Layout/TopMenu.js
+++ src/components/Layout/TopMenu.js
@@ -37,9 +37,11 @@ const TopMenu = () => {
               <Link to="#"><LanguageBar /></Link>
             </li> 
             */}
+            <li>
+              <ReadModeToggle />
+            </li>
           </ul>
         </nav>
-        <ReadModeToggle />
       </div>
     </>
   );
