2399fa7fbd3b9223a094adf11b75d64d3baded70
William Eduardo Meza <williameduardomeza@gmail.com>
2020-05-31 23:25:05 -0500

    feat: finish english about me page

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -41,10 +41,43 @@ function AboutMe({ data, location }) {
         </p>
         <h2>My passion</h2>
         <p>
-          Make websites, digital marketing, graphic design, painting, traveling, cooking, teaching.
-          I like science fiction movies and their books, old technology, disarm and repair
-          electrical, mechanical and electronic components, I’m fond of robotics and artificial
-          intelligence, martial arts and good football.
+          I love to design and code websites{' '}
+          <span role="img" aria-label="Computer">
+            🖥
+          </span>
+          , digital marketing, graphic design, painting{' '}
+          <span role="img" aria-label="Artist palette">
+             🎨
+          </span>
+          , traveling, cooking{' '}
+          <span role="img" aria-label="Chef">
+            👨‍🍳
+          </span>
+          , and teaching. I like science fiction movies and books{' '}
+          <span role="img" aria-label="Books">
+            📚
+          </span>
+          , old technology{' '}
+          <span role="img" aria-label="Radio">
+            📻
+          </span>
+          , disarm and repair electrical, mechanical and electronic components{' '}
+          <span role="img" aria-label="Electric pug">
+            🔌
+          </span>
+          , I’m big fan of robotics and artificial intelligence{' '}
+          <span role="img" aria-label="Mechanical arm">
+            🦾
+          </span>
+          , martial arts{' '}
+          <span role="img" aria-label="martial arts uniform">
+            🥋
+          </span>{' '}
+          and good football{' '}
+          <span role="img" aria-label="Soccer ball">
+            ⚽️
+          </span>
+          .
         </p>
         <h2>Dev Stack</h2>
         <h3>Web Frontend</h3>
@@ -53,8 +86,74 @@ function AboutMe({ data, location }) {
             <li>HTML5</li>
             <li>CSS3</li>
             <li>JavaScript</li>
-            <li>Webpack</li>
+            <li>React.js</li>
             <li>Vue.js</li>
+          </ul>
+        </p>
+        <h3>Web Backend</h3>
+        <p>
+          <ul>
+            <li>PHP</li>
+            <li>Laravel</li>
+            <li>NodeJS</li>
+            <li>Python</li>
+            <li>WordPress</li>
+          </ul>
+        </p>
+        <h3>Data</h3>
+        <p>
+          <ul>
+            <li>MySQL</li>
+            <li>PostgreSQL</li>
+            <li>MongoDB</li>
+            <li>Firebase</li>
+            <li>Redis</li>
+          </ul>
+        </p>
+        <h2>Me through time</h2>
+        <p>
+          <ul>
+            <li>
+              <strong>1993:</strong> I was born in Venezuela on November 14th. With 5 brothers
+              before and 3 after.
+            </li>
+            <li>
+              <strong>1999:</strong> My family buys my first PC with Windows XP.
+            </li>
+            <li>
+              <strong>Also 1999:</strong> My brother deletes the windows / system32 folder to make
+              more space for new games and I worry how to recover my installed games
+            </li>
+            <li>
+              <strong>2006:</strong> I disarmed my computer and I managed to arm it again (only one
+              screw left). I start making a living by formatting PCs.
+            </li>
+            <li>
+              <strong>2011:</strong> My father is left without a systems engineer. I learned php and
+              mysql on YouTube to replace it.
+            </li>
+            <li>
+              <strong>2012:</strong> I joined the UCLA Public University for computer engineering
+              thanks to my high grades.
+            </li>
+            <li>
+              <strong>2015:</strong> I began my service as a missionary in the church of Jesus
+              Christ of Latter-day Saints.
+            </li>
+            <li>
+              <strong>2017:</strong> I returned from my mission with honor. I got a job as a Web
+              Backend Developer in ONDigital.
+            </li>
+            <li>
+              <strong>2018:</strong> I married the love of my life Mayfra Useche{' '}
+              <span role="img" aria-label="Heart">
+                ♥️
+              </span>
+              .
+            </li>
+            <li>
+              <strong>2019:</strong> I was admitted to study in BYU Idaho College in USA.
+            </li>
           </ul>
         </p>
       </div>
