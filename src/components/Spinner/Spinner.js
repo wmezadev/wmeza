84febc7c571a7c28d6e36cac29e8cca77c784e9c
William Eduardo Meza <williameduardomeza@gmail.com>
2020-06-04 21:03:07 -0500

    feat: add spinner

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/self-closing-comp */
+import React from 'react';
+import './Spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Spinner;
